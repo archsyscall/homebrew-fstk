9a74d01fc5e19f135bfa82e949c21d5c92e1bc8a
Wansoo Kim <archsyscall@gmail.com>
2025-03-11 10:22:15 +0900

    Update fstk.rb

--- Formula/fstk.rb
+++ Formula/fstk.rb
@@ -2,7 +2,7 @@ class Fstk < Formula
   desc "File Stack - A CLI tool for managing files and directories in a stack format"
   homepage "https://github.com/archsyscall/fstk"
   url "https://github.com/archsyscall/fstk/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "ea6364ff3ffec69356abfeee9a35700923f0c985895f2e8e64d2e3eea04a2fd6"
+  sha256 "d2bdeb4a8bcf1aec99baa32895cec826b2b14d58c219f08acdd43d0894d3a570"
   license "MIT" # Update with your actual license
   
   depends_on "rust" => :build
